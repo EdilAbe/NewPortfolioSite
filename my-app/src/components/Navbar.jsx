cfb4f72a9ccc5d751b77a5459fc70e1a12ab82aa
EdilAbe <edilawittsehay@gmail.com>
2023-03-06 03:01:08 -0500

    responsive navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,11 +1,49 @@
-import React  from 'react';
+import React,{useState}  from 'react';
+import { motion } from "framer-motion"
+import {MdMenu, MdClose} from "react-icons/md";
 
+import "./Navbar.scss"
 
 const Navbar = () =>{
+    const [toggle, setToggle] = useState(false);
+
     return (
-        <div>
-            <nav>Hello there</nav>
-    </div>
+        
+            <nav className ="navbar">
+                
+                <div className="navbar-logo">
+        <h3>Edilawit Tsehay</h3>
+      </div>
+      <ul className="navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="navbar-list" key={`${item}`}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="navbar-menu">
+        <MdMenu onClick={() => setToggle(true)} /> 
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <MdClose onClick={() => setToggle(false)} /> 
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>     
+                
+                </nav>
         )
 }
 
